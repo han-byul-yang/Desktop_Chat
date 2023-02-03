88d6e9b9f9f0e28d9a12708e79d082289841f94d
Han Byul <yahb9@naver.com>
2023-02-03 17:51:52 +0900

    Feat: firebase에서 유저 정보 받아 띄워주는 UserList page 생성

--- renderer/pages/UserList/index.tsx
+++ renderer/pages/UserList/index.tsx
@@ -1,17 +1,39 @@
-import { useContext, useEffect } from 'react'
+import { useContext, useEffect, useState } from 'react'
 import { useRouter } from 'next/router'
+import { DocumentData } from 'firebase/firestore'
 
 import { UserStateContext } from 'pages/_app'
+import { getAllCollectionDocs } from 'services/firebaseService/firebaseDBService'
+import { IUserInfo } from 'types/dbDocType'
+import Header from 'components/Header'
+
+import styles from './userList.module.scss'
 
 const UserList = () => {
+  const [userInfoDocList, setUserInfoDocList] = useState<DocumentData[]>([])
   const userAuthState = useContext(UserStateContext)
   const navigate = useRouter()
+
+  useEffect(() => {
+    getAllCollectionDocs('userInfo').then((docData) => setUserInfoDocList(docData))
+  }, [])
 
   useEffect(() => {
     if (!userAuthState) navigate.push('/SignIn')
   }, [navigate, userAuthState])
 
-  return <div>userList</div>
+  return (
+    <>
+      <Header title='유저 목록' />
+      <ul className={styles.userList}>
+        {userInfoDocList.map((docData, index) => {
+          const docDataKey = `docData-${index}`
+
+          return <li key={docDataKey}>{docData.nickName}</li>
+        })}
+      </ul>
+    </>
+  )
 }
 
 export default UserList

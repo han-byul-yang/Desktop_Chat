3b73cdb3b7e8b7210fe6630dbcdada874f6901f6
Han Byul <yahb9@naver.com>
2023-04-03 18:24:12 +0900

    Remove: 코드 정리

--- src/routes/SignIn/index.tsx
+++ src/routes/SignIn/index.tsx
@@ -1,9 +1,5 @@
-/* eslint-disable import/no-extraneous-dependencies */
-import { ReactElement, useContext, useEffect } from 'react'
-import { useNavigate } from 'react-router-dom'
 import { useSetRecoilState } from 'recoil'
 
-// import { AuthStateContext } from 'pages/_app'
 import { signInAuth } from 'service/firebaseService/firebaseAuthService'
 import { errorMessageAtom } from 'Store/docInfoAtom'
 import { errorMessages } from 'constants/errorMessages'
@@ -11,16 +7,9 @@ import AuthContainer from 'components/AuthContainer'
 
 const SignIn = () => {
   const setErrorMessage = useSetRecoilState(errorMessageAtom)
-  // const userAuthState = useContext(AuthStateContext)
-  const navigate = useNavigate()
-
-  useEffect(() => {
-    // if (userAuthState) navigate('/UserList')
-  }, [navigate])
 
   const handleAuthSubmit = async (email: string, password: string) => {
     try {
-      // eslint-disable-next-line no-return-assign
       await signInAuth(email, password)
     } catch (error) {
       if (error instanceof Error) {
